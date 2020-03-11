c823c3d558a9d1ccaf0987f168f6baeb62a8c80b
JackLee <hlhan@vova.com.hk>
2020-03-11 19:09:02 +0800

    Pod install

---
+++ Example/Tests/UIColor+JKCategoriesSpec.m
@@ -0,0 +1,19 @@
+//
+//  UIColor+JKCategoriesSpec.m
+//  JKUIHelper
+//
+//  Created by JackLee on 2020/3/11.
+//  Copyright 2020 xindizhiyin2014. All rights reserved.
+//
+
+#import <Kiwi/Kiwi.h>
+#import "UIColor+JKCategories.h"
+
+
+SPEC_BEGIN(UIColor_JKCategoriesSpec)
+
+describe(@"UIColor+JKCategories", ^{
+
+});
+
+SPEC_END
